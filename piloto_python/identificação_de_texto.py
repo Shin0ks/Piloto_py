03ddce2fa64813ba822f876b1326925ced721f88
Shin0ks <sh1n0k@tutamail.com>
2026-07-04 12:10:13 -0300

    Update identificação_de_texto.py

--- piloto_python/identificação_de_texto.py
+++ piloto_python/identificação_de_texto.py
@@ -1,6 +1,12 @@
-#Coleta de dados
-print('Identificador de palavra')
-txt = str(input('Cole o texto :').strip())
 
-#Colocar oq quer verificar
-print ('Seu texto tem linux ? {}'.format('linux' in txt.lower()))
+# Coleta de dados
+print('Identificador de palavra')
+
+#Palavra chave
+key = str(input('Digite a palavra que quer achar: ')).strip().lower()
+
+#Texto
+txt = str(input('Cole o texto: ').strip().lower())
+
+#Resultado
+print(f'Seu texto tem a palavra '{key}'? {key in txt}')
