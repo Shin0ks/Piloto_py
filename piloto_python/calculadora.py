38fb88bec32af70e3dbdbdbaee5f51222f7d79ca
Shin0ks <sh1n0k@tutamail.com>
2026-07-22 12:43:16 -0300

    Update calculadora.py

--- piloto_python/calculadora.py
+++ piloto_python/calculadora.py
@@ -1,21 +1,16 @@
-#ENTRADA DE DADOS
-
+#Entrada de dados 
 n1 = int(input('Digite um numero :'))
 n2 = int(input('Digite outro numero :'))
 
-#CALCULADORA
-
+#Calculadora 
 ss = n1 + n2
-
 sm = n1 * n2
-
 tt = ss + sm
 
-#RESULTADO
-
+#Resultado
 print(f'a some entre {n1} e {n2} é igual a : {ss}')
-
 print (f'a multiplicação entre {n1} e {n2} é igual a : {sm}')
-
 print('e o total entre soma e multiplicação é' , tt)
+
+#Mais um projeto simples e clássico. Basta escolher dois valores que sairá o resultado depois. 
 
