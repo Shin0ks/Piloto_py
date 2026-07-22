2873649581feea00ef1883dab8f42f0d5a980810
Shin0ks <sh1n0k@tutamail.com>
2026-07-22 12:45:51 -0300

    Update Raiz_quadrada.py

--- piloto_python/Raiz_quadrada.py
+++ piloto_python/Raiz_quadrada.py
@@ -3,8 +3,9 @@ import math
 
 #coleta
 num1 = float(input('Digite um numero :'))
-
 raiz = math.sqrt(num1)
 
 #Saida
 print('A raiz do numero {} é : {:.2f}'.format(num1 , raiz ))
+
+#Coloque um número e descubra o valor da sua raiz quadrada. 
