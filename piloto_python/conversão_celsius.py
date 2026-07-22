48f42abe3377d5297c6ed135b3bab49fa8b71612
Shin0ks <sh1n0k@tutamail.com>
2026-07-22 12:37:02 -0300

    Update conversão_celsius.py

--- piloto_python/conversão_celsius.py
+++ piloto_python/conversão_celsius.py
@@ -8,3 +8,5 @@ k = (celsius + 273)
 #Resultado
 print(f'a conversão de celsius para farenheit é : {f}')
 print(f'a conversão de celsius para kelvin é : {k}')
+
+#Um clássico projeto de conversão de temperatura.Não tem muito o que falar kkkk. 
