d10393e67164b459440bfef655eff26f2809448f
Shin0ks <sh1n0k@tutamail.com>
2026-07-22 12:29:26 -0300

    Update and rename piloto_python/identificação_de_texto.py to Caça_palavra.py

--- Caça_palavra.py
+++ Caça_palavra.py
@@ -10,3 +10,6 @@ txt = str(input('Cole o texto: ').strip().lower())
 
 #Resultado
 print(f'Seu texto tem a palavra '{key}'? {key in txt}')
+
+#Código bem simples, consiste em achar uma palavra específica dentro de um texto. 
+#Como funciona? basta digitar a palavra que queira achar, logo depois o texto escolhido. 
